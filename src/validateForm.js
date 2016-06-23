81774bc1cdf379c3fd12ef1bd403ddf23b2c4806
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-06-22 21:19:07 -0700

    Added email regex

--- src/validateForm.js
+++ src/validateForm.js
@@ -15,11 +15,17 @@ export const validateName = (name) => {
 };
 
 export const validateEmail = (email) => {
-  // TODO add regex to check email
   const label = email.labels[0];
+  const re = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/ // eslint-disable-line
   if (email.value.length === 0) {
     email.style.boxShadow = '0 0 0 3px red';
     label.innerHTML = 'Email Required';
+    label.style.color = 'red';
+    return false;
+  }
+  if (!re.test(email.value)) {
+    email.style.boxShadow = '0 0 0 3px red';
+    label.innerHTML = 'Email format "user@example.com" required';
     label.style.color = 'red';
     return false;
   }
