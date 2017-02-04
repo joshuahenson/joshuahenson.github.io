2b6041de5e09086ecece09dd08fac32ec1cc0919
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-02-03 18:13:56 -0800

    Added querySelector for form labels
    Didn't realize that firefox couldn't access labels on target

--- src/validateForm.js
+++ src/validateForm.js
@@ -1,7 +1,11 @@
 /* eslint no-param-reassign: 0 */
 
+const nameLabel = document.querySelector('[for=name]');
+const emailLabel = document.querySelector('[for=email]');
+const messageLabel = document.querySelector('[for=message]');
+
 export const validateName = (name) => {
-  const label = name.labels[0];
+  const label = nameLabel;
   if (name.value.length === 0) {
     name.style.boxShadow = '0 0 0 3px red';
     label.innerHTML = 'Name Required';
@@ -15,7 +19,7 @@ export const validateName = (name) => {
 };
 
 export const validateEmail = (email) => {
-  const label = email.labels[0];
+  const label = emailLabel;
   const re = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/ // eslint-disable-line
   if (email.value.length === 0) {
     email.style.boxShadow = '0 0 0 3px red';
@@ -36,7 +40,7 @@ export const validateEmail = (email) => {
 };
 
 export const validateMessage = (message) => {
-  const label = message.labels[0];
+  const label = messageLabel;
   if (message.value.length === 0) {
     message.style.boxShadow = '0 0 0 3px red';
     label.innerHTML = 'Message Required';
