f93ba9a11150600c9fd413c7c1c8adc9a83f4c2e
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-11-12 08:51:18 -0800

    Set up webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-
-// TODO: Update the following in prod
-// extract styles
-// babel
-// dist to docs
-// remove hmr https://webpack.js.org/guides/hot-module-replacement/
 
 module.exports = {
   entry: './src/js/index.js',
   devtool: 'cheap-module-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: './docs',
     hot: true
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']), // TODO: change to docs after testing
     new HtmlWebpackPlugin({
       inject: true,
       template: 'src/html/index.html',
@@ -27,7 +19,7 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist') // TODO: change to docs after testing
+    path: path.resolve(__dirname, 'docs')
   },
   module: {
     rules: [
@@ -35,9 +27,11 @@ module.exports = {
         test: /\.scss$/,
         use: [{
           loader: "style-loader"
-        }, {
+        },
+        {
           loader: "css-loader"
-        }, {
+        },
+        {
           loader: "sass-loader"
         }]
       }
---
+++ webpack.config.prod.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+const flexbugs = require('postcss-flexbugs-fixes');
+
+module.exports = {
+  entry: './src/js/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['docs']),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/html/index.html',
+      minify: {
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeEmptyAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    new UglifyJSPlugin(),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash:8].css',
+    })
+  ],
+  output: {
+    filename: '[name].[chunkhash:8].js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              minimize: true
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9',
+                  ],
+                  flexbox: 'no-2009',
+                }),
+                flexbugs()
+              ]
+            }
+          },
+          {
+            loader: "sass-loader"
+          }]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
